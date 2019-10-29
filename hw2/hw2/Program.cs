dc4c55ae10b365faac309a1f52955712a0582121
Nathan Ingleton <NIngleton@spartaglobal.com>
2019-10-29 17:01:49 +0000

    5 0clock edit

--- hw2/hw2/Program.cs
+++ hw2/hw2/Program.cs
@@ -6,10 +6,63 @@ using System.Threading.Tasks;
 
 namespace hw2
 {
+    class Vehicle
+    {
+        double topSpeed;
+       private int engineSize;
+       
+        string colour;
+        double weight;
+        public void VehicleSound()
+        {
+            Console.WriteLine("Baaaaaaang");
+        }
+
+        void setEngineSize(int value)
+        {
+            engineSize = value;
+        }
+
+        int getEngineSize(int value)
+        {
+            return engineSize;
+        }
+    }
+     class Car: Vehicle
+    {
+        bool IsConvertible;
+        
+            
+        
+
+        public void VehicleSound()
+        {
+            Console.WriteLine("Vroooom");
+        }
+    }
+    class MotorBike : Vehicle
+    {
+
+        public void VehicleSound()
+        {
+            Console.WriteLine("Broooom");
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            MotorBike MB = new MotorBike();
+            Vehicle veh = new Vehicle();
+
+
+            veh.VehicleSound();
+            car.VehicleSound();
+            MB.VehicleSound();
+            
+            Console.ReadLine();
         }
     }
 }
